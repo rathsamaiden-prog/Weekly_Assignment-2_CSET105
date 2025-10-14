fcfc25562d14fccab6817241d412342637bd1ba4
Aiden Rathsam <rathsamaiden@gmail.com>
2025-10-14 10:32:42 -0400

    template change kinda implimented

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
+let tempGame = document.getElementsByClassName(`gameTemplate`)[0];
+let cloneGame = tempGame.content.cloneNode(true);
+document.body.appendChild(cloneGame);
+
 let player = {input: null, wins: null}
 let computer = {input: null, wins: null}
 function moveSelect(btn){
     player.input = btn.id
 }
 
+
+let round = `Player - ${playerInput}, Computer - ${computerInput}`
+let ties = null
+let score = `Score: Player - ${player.wins}, Computer - ${computer.wins}, Tie - ${ties}`;
 let inputType = [`rock`, `paper`, `scissor`]
 function shoot(){
     let randSelect = inputType[Math.floor((Math.random()*3))]
     computer.input = randSelect
+
+    if(player.input === computer.input)
+        ties++
+    else if(player.input === `rock` && computer.input === `scissor` ||
+        player.input === `paper` && computer.input === `rock` ||
+        player.input === `scissor` && computer.input === `paper`
+    )
+        player.wins++
+    else
+        computer.wins++
+
+    document.body.innerHTML = ``
+    let tempResult = document.getElementsByClassName(`resultTemplate`)[0];
+    let cloneResult = tempResult.content.cloneNode(true);
+    document.body.appendChild(cloneResult);
 }
 
-let round = `Player - ${playerInput}, Computer - ${computerInput}`
 
-let ties = null
-if(player.input === computer.input)
-    ties++
-else if(player.input === `rock` && computer.input === `scissor` ||
-    player.input === `paper` && computer.input === `rock` ||
-    player.input === `scissor` && computer.input === `paper`
-)
-    player.wins++
-else
-    computer.wins++
-let score = `Score: Player - ${player.wins}, Computer - ${computer.wins}, Tie - ${ties}`;
+
+function playGame(){
+    document.body.innerHTML = ``
+    let tempGame = document.getElementsByClassName(`gameTemplate`)[0];
+    let cloneGame = tempGame.content.cloneNode(true);
+    document.body.appendChild(cloneGame);
+}
